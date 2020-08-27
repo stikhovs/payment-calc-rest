448dc388ebfd448d574708acdcaa43490237f8a5
Sergey Tikhov <stikhovs@gmail.com>
2020-08-27 12:13:59 +0300

    Created repository

---
+++ src/main/java/ru/payment/calc/payment_calculator/PaymentCalculatorApplication.java
@@ -0,0 +1,13 @@
+package ru.payment.calc.payment_calculator;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PaymentCalculatorApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(PaymentCalculatorApplication.class, args);
+    }
+
+}
---
+++ src/test/java/ru/payment/calc/payment_calculator/PaymentCalculatorApplicationTests.java
@@ -0,0 +1,13 @@
+package ru.payment.calc.payment_calculator;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PaymentCalculatorApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
