1ab1a8370de938e2535ca5ee82c4875baf5a704c
Sergei Tikhov <s.tikhov@unabank.com>
2023-06-02 19:27:43 +0300

    Add backend url env var for NoIdlingConfig

--- src/main/java/ru/payment/calc/payment_calculator/config/NoIdlingConfig.java
+++ src/main/java/ru/payment/calc/payment_calculator/config/NoIdlingConfig.java
@@ -2,6 +2,7 @@ package ru.payment.calc.payment_calculator.config;
 
 import lombok.SneakyThrows;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Profile;
 import org.springframework.scheduling.annotation.EnableScheduling;
@@ -14,11 +15,14 @@ import org.springframework.web.client.RestTemplate;
 @EnableScheduling
 public class NoIdlingConfig {
 
+    @Value("${backend.url}")
+    private String backendUrl;
+
     @SneakyThrows
     @Scheduled(fixedRate = 600000L) // 10 min
     public void pingGoogle() {
         RestTemplate restTemplate = new RestTemplate();
-        Object obj = restTemplate.getForObject("https://payment-calculator-backend.onrender.com/actuator/health", Object.class);
+        Object obj = restTemplate.getForObject(backendUrl + "/actuator/health", Object.class);
         log.info("Self ping {}", obj);
     }
 
